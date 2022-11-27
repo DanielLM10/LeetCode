8fdc36b5c800e63558c344731e1fa90875b1131f
daniel <853567524@qq>
2022-11-27 17:49:54 +0100

    2022.11.27

--- 算法/2.双指针/有序数组的平方.py
+++ 算法/2.双指针/有序数组的平方.py
@@ -4,6 +4,14 @@
 输入:nums = [-4,-1,0,3,10]; 输出:[0,1,9,16,100]
 输入:nums = [-7,-3,2,3,11]; 输出:[4,9,9,49,121]
 """
-class Solution:
-    def sortedSquares(self, nums: List[int]) -> List[int]:
-        return sorted(num * num for num in nums)
+from typing import List
+
+
+def sortedSquares(num: List[int]) -> List[int]:
+    return sorted(num_ * num_ for num_ in num)
+
+
+nums = [-4, -1, 0, 3, 10]
+print(sortedSquares(nums))
+nums = [-7, -3, 2, 3, 11]
+print(sortedSquares(nums))
